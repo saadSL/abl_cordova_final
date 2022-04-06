1d4d4469f6f8d64592df484ab27b2aac9dbd36bb
Usman Aslam <usmanas.lam@systemsltd.com>
2022-04-06 16:59:50 +0500

    6/4/22 4.59 p.m usman

--- app/src/main/java/com/unikrew/faceoff/ABLPlugin/model/phase2/get_consumer_account_details/GetConsumerAccountDetailsPostParams.java
+++ app/src/main/java/com/unikrew/faceoff/ABLPlugin/model/phase2/get_consumer_account_details/GetConsumerAccountDetailsPostParams.java
@@ -1,4 +1,15 @@
 package com.unikrew.faceoff.ABLPlugin.model.phase2.get_consumer_account_details;
 
-public class GetConsumerAccountDetailsPostParams {
+import java.io.Serializable;
+
+public class GetConsumerAccountDetailsPostParams implements Serializable {
+    public GetConsumerAccountDetailsPostData data;
+
+    public GetConsumerAccountDetailsPostData getData() {
+        return data;
+    }
+
+    public void setData(GetConsumerAccountDetailsPostData data) {
+        this.data = data;
+    }
 }
---
+++ app/src/main/java/com/unikrew/faceoff/ABLPlugin/model/phase2/get_consumer_account_details/GetConsumerAccountDetailsResponseData.java
@@ -0,0 +1,6 @@
+package com.unikrew.faceoff.ABLPlugin.model.phase2.get_consumer_account_details;
+
+import java.io.Serializable;
+
+public class GetConsumerAccountDetailsResponseData implements Serializable {
+}
